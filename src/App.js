c2ceb9b3f68934201621e80ef89c11d403eeeabd
sdossey <sdossey@techtalentsouth.com>
2023-03-08 18:47:25 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  let [petList, setPetList]=React.useState([]);
+  let [petName, setPetName]=React.useState("");
+  let [petBreed, setPetBreed]=React.useState("");
+  let [petAge, setPetAge]=React.useState(0);
+
+  function handleNameInput(event) {
+    setPetName(event.target.value)
+  }
+
+  function handleBreedInput(event) {
+    setPetBreed(event.target.value)
+  }
+  
+  function handleAgeInput(event) {
+    setPetAge(event.target.value)
+  }
+
+  function handleClick() {
+    let petObject = {"petName":petName, "petBreed":petBreed, "petAge":petAge};
+    setPetList( [...petList, petObject]);
+    setPetName("");
+    setPetBreed("");
+    setPetAge(0);
+  }
+
+  let petElementArray = petList.map((animal, index) => (
+    <div key={index}>
+      <div>Name: {animal.petName}</div>
+      <div>Breed: {animal.petBreed}</div>
+      <div>Age: {animal.petAge}</div>
+    </div>
+  ));
+
+  return (  
+    <div>
+      <h4>Pet Store Animal List</h4>
+      {petElementArray}
+      <input value={petName} 
+            onChange={handleNameInput} type="text" placholder="Pet Name"/>
+      <input value={petBreed} 
+            onChange={handleBreedInput} type="text" placholder="Pet Breed"/>
+      <input value={petAge} 
+            onChange={handleAgeInput} type="text" placholder="Pet Age"/>                          
+      <button onClick={handleClick}>Add Animal</button>
     </div>
   );
 }
